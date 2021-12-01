8d6c07b93da7ec2fcf2293b88e36a467e5e55eae
Drew DePriest <drew.depriest@gmail.com>
2021-11-30 22:28:47 -0600

    first commit

---
+++ 2020/20201201.py
@@ -0,0 +1,22 @@
+import os
+import itertools
+
+# Advent of Code 2020
+# Day 01
+# https://adventofcode.com/2020/day/1
+
+# set path to local folder
+localFolder = os.getcwd() + "/2020/"
+
+arr = []
+
+with open(localFolder + '20201201input.txt', "r") as f:
+  for line in f:
+    stripped_line = line.strip()
+    arr.append(int(stripped_line))
+
+prod = 0
+for numbers in itertools.combinations(arr,2):
+    if sum(numbers) == 2020:
+        prod = numbers[0]*numbers[1]
+        print("product: " + str(prod)
